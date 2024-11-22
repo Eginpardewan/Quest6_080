0d164038aaf86a7db24d4c4e40108fb54b5054df
Egin Pardewan <eginpardewan@gmail.com>
2024-11-22 21:33:24 +0700

    membuat object matakuliah

---
+++ app/src/main/java/com/example/activity61/data/MataKuliah.kt
@@ -0,0 +1,12 @@
+package com.example.activity61.data
+
+object MataKuliah {
+    val option = listOf(
+        "Agama Islam dan Kemuhammadiyahan",
+        "Pengambangan Aplikasi Web",
+        "Pancasila",
+        "Dasar Teknologi Informasi",
+        "Fisika dan Elektronika",
+        "Logika Teknik Pemrograman",
+    )
+}
